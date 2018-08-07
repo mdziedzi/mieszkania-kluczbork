1c720195f207bcb03dafb7d314d52068cba66259
mdziedzi <marcindziedzic8@gmail.com>
2018-08-07 11:05:32 +0200

    out2

---
+++ out/assets/js/main.js
@@ -0,0 +1,13 @@
+// $(window).on('resize', function () { 
+//     $('body').css('padding-top', parseInt($('#my-navbar').css("height"))+100);
+// });
+
+// $(window).on('load', function () { 
+//     $('body').css('padding-top', parseInt($('#my-navbar').css("height"))+100);        
+// });
+
+// $(window).on('load', function myFunction() {
+// document.body.style.backgroundColor = "red";
+// });
+
+window.addEventListener("hashchange", function() { scrollBy(0, -55) })
